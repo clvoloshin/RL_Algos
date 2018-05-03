7d70f156e19a764a676988420ff3320dc8034729
Cameron Voloshin <cameron@ekistic.com>
2018-05-02 21:07:19 -0500

    Registering new environment

--- gym-snake/gym_snake/__init__.py
+++ gym-snake/gym_snake/__init__.py
@@ -4,3 +4,11 @@ register(
     id='snake-v0',
     entry_point='gym_snake.envs:SnakeEnv',
 )
+
+register(
+    id='snake-v1',
+    entry_point='gym_snake.envs:SnakeEnv',
+    screen_width = 25, 
+    screen_height=25, 
+    n_actors = 2,
+)
